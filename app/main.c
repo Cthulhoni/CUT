9e33ad69ff2e23be96896ac2f036887a6bd86f6b
cthulhoni <zero.sbntu@gmail.com>
2021-06-26 18:24:11 +0200

    Logger added to main

--- app/main.c
+++ app/main.c
@@ -7,9 +7,11 @@
 #include <reader.h>
 #include <analyzer.h>
 #include <printer.h>
+#include <watchdog.h>
+#include <logger.h>
 #include <string_buffer.h>
 
-#define NUM_OF_THREADS 3U
+#define NUM_OF_THREADS 4U
 
 static pthread_t threads[NUM_OF_THREADS];
 static pthread_t watchdog;
@@ -17,10 +19,12 @@ static pthread_t watchdog;
 static watch_struct* w_struct = NULL;
 static string_buffer* sbuf = NULL;
 static string_buffer* print_buf = NULL;
+static string_buffer* log_buf = NULL;
 static reader_args* r_args = NULL;
 static analyzer_args* a_args = NULL;
 static printer_args* p_args = NULL;
 static watchdog_args* w_args = NULL;
+static logger_args* l_args = NULL;
 
 static void sigterm_handler(int sig) {
 
@@ -34,9 +38,11 @@ static void sigterm_handler(int sig) {
 static void cleanup(void) {
     string_buffer_destroy(sbuf);
     string_buffer_destroy(print_buf);
+    string_buffer_destroy(log_buf);
     printer_args_destroy(p_args);
     analyzer_args_destroy(a_args);
     reader_args_destroy(r_args);
+    logger_args_destroy(l_args);
     watch_struct_destroy(w_struct);
     watchdog_args_destroy(w_args);
 }
@@ -55,25 +61,32 @@ int main(void) {
 
     sbuf = string_buffer_create();
     print_buf = string_buffer_create();
-    if (!sbuf || !print_buf)
+    log_buf = string_buffer_create();
+    if (!sbuf || !print_buf || !log_buf)
         return -1;
-    r_args = reader_args_create(sbuf, w_struct);
-    a_args = analyzer_args_create(sbuf, print_buf, w_struct);
-    p_args = printer_args_create(print_buf, w_struct);
-    if (!r_args || !a_args || !p_args)
+    r_args = reader_args_create(sbuf, w_struct, log_buf);
+    a_args = analyzer_args_create(sbuf, print_buf, w_struct, log_buf);
+    p_args = printer_args_create(print_buf, w_struct, log_buf);
+    l_args = logger_args_create(log_buf);
+    if (!r_args || !a_args || !p_args || !l_args)
         return -1;
 
     pthread_t reader = 0;
     pthread_t analyzer = 0;
     pthread_t printer = 0;
+    
+    pthread_t logger = 0;
 
     pthread_create(&reader, NULL, printer_print, (void*)p_args);
     pthread_create(&analyzer, NULL, analyzer_process_cpu_data, (void*)a_args);
     pthread_create(&printer, NULL, reader_get_cpu_data, (void*)r_args);
 
+    pthread_create(&logger, NULL, logger_print, (void*)l_args);
+
     threads[0] = printer;
     threads[1] = analyzer;
     threads[2] = reader;
+    threads[3] = logger;
 
     w_args = watchdog_args_create(w_struct, NUM_OF_THREADS, threads);
 
@@ -87,9 +100,12 @@ int main(void) {
     pthread_join(printer, NULL);
 
 
-    if (!watch_struct_is_running(w_struct))
-    pthread_cancel(watchdog);
+    if (!watch_struct_is_running(w_struct)) {
+        pthread_cancel(logger);
+        pthread_cancel(watchdog);
+    }
     
+    pthread_join(logger, NULL);
     pthread_join(watchdog, NULL);
     
     return 0;
